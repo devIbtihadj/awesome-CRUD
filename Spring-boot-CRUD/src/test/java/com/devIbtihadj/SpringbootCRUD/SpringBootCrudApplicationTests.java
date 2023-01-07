06515271546f27d4c02048865972a1216ecbf067
ibtihadj <ibtihadj.dev@gmail.com>
2023-01-07 09:30:34 +0000

    first commit

---
+++ Spring-boot-CRUD/src/test/java/com/devIbtihadj/SpringbootCRUD/SpringBootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devIbtihadj.SpringbootCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
